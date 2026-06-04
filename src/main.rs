6c2994cdefbf84315a825770b8a8259c38faca00
yoyosmu <jun.mu57@gmail.com>
2026-06-03 21:25:26 -0400

    seperated the sorter and main functions into two files for easir compiling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
 use serde::Deserialize;
-use std::fs;
 use std::thread;
 use std::time::Duration;
+mod sorter;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -74,129 +74,12 @@ fn main() -> std::io::Result<()> {
 
     if args.watch {
         loop {
-            run_once(&args)?;
+            sorter::main(&args)?;
             thread::sleep(Duration::from_secs(1));
         }
     } else {
-        run_once(&args)?;
+        sorter::main(&args)?;
     }
-
-    Ok(())
-}
-
-fn run_once(args: &Args) -> std::io::Result<()> {
-    let config_dir = dirs::config_dir().unwrap().join("file_sorter");
-    let config_path = config_dir.join("Sorter.toml");
-
-    let downloads = dirs::download_dir().unwrap_or_else(|| std::env::current_dir().unwrap());
-
-    let folder = args
-        .folder
-        .clone()
-        .map(std::path::PathBuf::from)
-        .unwrap_or(downloads);
-
-    let paths = fs::read_dir(&folder)?;
-
-    let mut docs_count = 0;
-    let mut images_count = 0;
-    let mut audio_count = 0;
-    let mut video_count = 0;
-    let mut archive_count = 0;
-
-    let pdf_dir = folder.join("PDFs");
-    let img_dir = folder.join("IMGs");
-    let audio_dir = folder.join("AUDIOs");
-    let video_dir = folder.join("VIDs");
-    let archive_dir = folder.join("ARCHIVEs");
-
-    let images = [
-        "jpg", "png", "webp", "jpeg", "gif", "avif", "tiff", "bmp", "raw", "heif", "heic",
-    ];
-    let docs = [
-        "docx", "pdf", "doc", "odt", "txt", "rtf", "xps", "xlsx", "csv", "ods",
-    ];
-    let audio = [
-        "mp3", "wav", "aac", "flac", "ogg", "wma", "aiff", "m4a", "dts", "opus",
-    ];
-    let video = [
-        "mp4", "avi", "mov", "webm", "flv", "wmv", "mpg", "mpeg", "3gp", "3g2", "m4v", "mkv",
-    ];
-    let archive = [
-        "zip", "tar", "gz", "bz2", "7z", "rar", "xz", "lzh", "lha", "taz", "pkg", "deb", "tgz",
-        "lzip",
-    ];
-
-    if !config_path.exists() {
-        fs::create_dir_all(&config_dir)?;
-        fs::write(&config_path, DEFAULT_CONFIG)?;
-        println!("Created config: {:?}", config_path);
-    }
-
-    let config_text = fs::read_to_string(&config_path)?;
-    let config: Config = toml::from_str(&config_text).unwrap_or_default();
-
-    for entry in paths {
-        if let Ok(entry) = entry {
-            let path = entry.path();
-
-            if path.is_dir() {
-                continue;
-            }
-
-            let file_name = match path.file_name() {
-                Some(name) => name,
-                None => continue,
-            };
-
-            if let Some(ext) = path.extension() {
-                let ext = ext.to_string_lossy().to_lowercase();
-
-                let dest = if config.features.documents && docs.contains(&ext.as_str()) {
-                    docs_count += 1;
-                    fs::create_dir_all(&pdf_dir)?;
-                    pdf_dir.join(file_name)
-                } else if config.features.images && images.contains(&ext.as_str()) {
-                    images_count += 1;
-                    fs::create_dir_all(&img_dir)?;
-                    img_dir.join(file_name)
-                } else if config.features.audio && audio.contains(&ext.as_str()) {
-                    audio_count += 1;
-                    fs::create_dir_all(&audio_dir)?;
-                    audio_dir.join(file_name)
-                } else if config.features.video && video.contains(&ext.as_str()) {
-                    video_count += 1;
-                    fs::create_dir_all(&video_dir)?;
-                    video_dir.join(file_name)
-                } else if config.features.archives && archive.contains(&ext.as_str()) {
-                    archive_count += 1;
-                    fs::create_dir_all(&archive_dir)?;
-                    archive_dir.join(file_name)
-                } else {
-                    continue;
-                };
-
-                if args.dry_run {
-                    println!(
-                        "[DRY RUN] {:?} -> {:?}",
-                        path.file_name().unwrap(),
-                        dest.file_name().unwrap()
-                    );
-                } else {
-                    fs::rename(&path, &dest)?;
-                }
-            }
-        }
-    }
-
-    let total = docs_count + images_count + audio_count + video_count + archive_count;
-
-    println!("Documents: {}", docs_count);
-    println!("Images: {}", images_count);
-    println!("Audio: {}", audio_count);
-    println!("Video: {}", video_count);
-    println!("Archives: {}", archive_count);
-    println!("Total: {}", total);
 
     Ok(())
 }
